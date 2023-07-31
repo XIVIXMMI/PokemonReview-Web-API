873d2918d1aa4af7ee189192f202fc973befe77a
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-07-31 13:15:15 +0700

    Many-To-Many Relationships Create Join Table

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -6,6 +6,9 @@ namespace PokemonReviewApp.Models
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public DateTime BirthDay { get; set; }
+		public ICollection<Review> Reviews { get; set; }
+		public ICollection<PokemonOwner> PokemonOwners { get; set; }
+		public ICollection<PokemonCategory> PokemonCategories { get; set; }
 	}
 }
 
--- PokemonReviewApp/Models/Reviewer.cs
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -6,6 +6,7 @@ namespace PokemonReviewApp.Models
 		public int Id { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+		public ICollection<Review> Reviews { get; set;	}
 	}
 }
 
