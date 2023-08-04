4db466c4719600cd1d69bf7f6f0158bdaf0a5475
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-08-04 13:18:52 +0700

    Create Dto models

--- PokemonReviewApp/Repository/PokemonRepository.cs
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -12,10 +12,38 @@ namespace PokemonReviewApp.Repository
 		{
 			_context = context;
 		}
-		public ICollection<Pokemon> GetPokemons()
+
+        public Pokemon GetPokemon(int id)
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return _context.Pokemons.Where(p => p.Name == name).FirstOrDefault();
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+            if (review.Count() <= 0)
+                return 0;
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
+        public ICollection<Pokemon> GetPokemons()
 		{
 			return _context.Pokemons.OrderBy(p => p.Id).ToList();
 		}
-	}
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemons.Any(p => p.Id == pokeId);
+        }
+    }
 }
 
