2621094961c3c109f41df78861c959739ab846fd
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-08-07 00:24:59 +0700

    create country

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -15,8 +15,8 @@ builder.Services.AddControllers().AddJsonOptions(x =>
                 x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
-//builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-//builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 //builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
 //builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
 //builder.Services.AddScoped<IReviewerRepository, ReviewerRepository>();
