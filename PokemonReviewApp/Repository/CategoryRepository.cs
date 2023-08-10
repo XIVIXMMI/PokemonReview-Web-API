939925d6f38bd283fdc5c7b646a1e9c96c63b6d3
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-08-10 23:44:35 +0700

    Update & Create Method for Category

--- PokemonReviewApp/Repository/CategoryRepository.cs
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -19,6 +19,17 @@ namespace PokemonReviewApp.Repository
             return _context.Categories.Any(c => c.Id == id);
         }
 
+        public bool CreateCategory(Category category)
+        {
+            _context.Add(category);
+            return Save();
+        }
+
+        public bool DeleteCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
         public ICollection<Category> GetCategories()
         {
             return _context.Categories.ToList();
@@ -36,6 +47,18 @@ namespace PokemonReviewApp.Repository
             return _context.PokemonCategories.Where(c => c.CategoryId == categoryId)
                 .Select(c => c.Pokemon)
                 .ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            _context.Update(category);
+            return Save();
         }
     }
 }
